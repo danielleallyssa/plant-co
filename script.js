15bf5a55a878b973825048daca02fd74635a40b1
Danielle Brown <hello@danielleallyssa.com>
2023-02-16 03:08:19 -0500

    Html and CSS set up. Added JS toggle feature

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// HTML Selections
+const mainTag = document.querySelector("main");
+const menuTag = document.querySelector(".toggle-nav");
 
+// menu toggle action
+const showMenu = () => mainTag.classList.toggle("show-menu");
+
+// click event
+menuTag.addEventListener("click", function () {
+  showMenu();
+});
